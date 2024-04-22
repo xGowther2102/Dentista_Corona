faa6e64bfe618772c10d9b9e35f32d95cfc5b179
edwin hp <edwin.peralta@itsvc.edu.mx>
2024-04-22 01:18:34 -0600

    fix graph

---
+++ pruebas_edwin/sidebar_con_pagina.php
@@ -0,0 +1,7 @@
+<?php require_once '../pruebas_edwin/test2.php'; ?>
+
+<div class="main-content">
+    <!-- Contenido principal -->
+    <?php require_once './grapich.php' ; ?>
+</div>
+<div class="content-strip"></div>
